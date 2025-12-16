64ba31cbea212c60aca52adcf2c74647262d2c84
unknown <mraj110101@gmail.com>
2025-12-16 13:39:59 +0530

    https://github.com/mraj110101-commits/hospital.git

--- HospitalManagement/settings.py
+++ HospitalManagement/settings.py
@@ -75,13 +75,16 @@ WSGI_APPLICATION = 'HospitalManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'hospital management',
+        'USER':'postgres',
+        'PASSWORD':'MOHANSURYA09',
+        'HOST':'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
